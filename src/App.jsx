483980fac73050d4174d39e78305c24cb6de41bd
David Regaldi <david.regaldi@gmail.com>
2025-12-21 22:54:37 +0100

    First Steps Towards Our Tic-Tac-Toe Game.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@
 function App() {
 
 
-  return (
-    <p>Coming Soon...</p>
-  )
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+      GAMEBOARD
+    </div>
+  </main>;
 }
 
 export default App
